61304d1e64be4245a8e00629a320dc5ae9c761a8
KKTA22 <kushal.kant@tigeranalytics.com>
2023-01-10 00:19:47 +0530

    added google login

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import Login from './components/Login';
+import App from './App';
+import firebase from './services/firebase';
+
+import './App.css';
+
+
+
+function Home() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setUser(user);
+    })
+  }, [])
+
+  console.log(user);
+
+  return (
+    <div className="app" style={{display:"flex"}}>
+      <h1>Welcome to Shopcart</h1>
+      {user ? <App user={user}/>:<Login />}
+    </div>
+  );
+}
+
+export default Home;
